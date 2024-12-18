0047e43245324966b5dd7a1e5672d5f73ed1e019
yatsuna827 <yatsuna_827@yahoo.co.jp>
2024-12-19 02:29:57 +0900

    feat: Loadingをぐるぐるに変えた

---
+++ app/components/LoadingCircle.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/cn'
+
+const Container: React.FC<React.ComponentProps<'div'>> = ({ className, ...props }) => {
+  return (
+    <div
+      className={cn('flex justify-center items-center', className)}
+      aria-label="loading"
+      {...props}
+    />
+  )
+}
+
+const Spinner: React.FC<React.ComponentProps<'div'>> = ({ className, ...props }) => {
+  return (
+    <div
+      className={cn(
+        'animate-spin h-10 w-10 border-4 border-blue-300 rounded-full border-t-transparent',
+        className,
+      )}
+      {...props}
+    />
+  )
+}
+
+export const LoadingCircle = {
+  Container,
+  Spinner,
+}
