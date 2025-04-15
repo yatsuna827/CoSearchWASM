d81252dc1e16ceaf0de427251b3566fe6eb57d98
yatsuna827 <yatsuna_827@yahoo.co.jp>
2025-04-15 23:05:42 +0900

    feat: Sheetコンポーネントを追加

--- app/components/LabeledInput.tsx
+++ app/components/LabeledInput.tsx
@@ -1,17 +1,15 @@
-import { type ComponentProps, forwardRef, useId } from 'react'
+import { useId } from 'react'
 
-type LabeledInputProps = ComponentProps<'input'> & { label: string }
-export const LabeledInput = forwardRef<HTMLInputElement, LabeledInputProps>(
-  ({ label, ...props }, ref) => {
-    const id = useId()
+type Props = React.ComponentProps<'input'> & { label: string }
+export const LabeledInput: React.FC<Props> = ({ label, ref, ...props }) => {
+  const id = useId()
 
-    return (
-      <div>
-        <label htmlFor={id} className="block text-sm text-[#333]/80 mb-1 select-none">
-          {label}
-        </label>
-        <input {...props} ref={ref} id={id} />
-      </div>
-    )
-  },
-)
+  return (
+    <div>
+      <label htmlFor={id} className="block text-sm text-[#333]/80 mb-1 select-none">
+        {label}
+      </label>
+      <input {...props} ref={ref} id={id} />
+    </div>
+  )
+}
