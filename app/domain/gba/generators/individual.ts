2637ea9ec49112644782a304ffd1d151b412d190
yatsuna827 <yatsuna_827@yahoo.co.jp>
2025-08-14 23:02:33 +0900

    feat: 固定シンボルの生成処理を実装

---
+++ app/domain/gba/generators/individual.ts
@@ -0,0 +1,22 @@
+import { Ref } from '@/utilities/ref'
+import { type LCG } from '../lcg'
+import { IVs, SchemeName, generateIVs } from './ivs'
+import { generatePID } from './pid'
+
+export type IndividualBase = {
+  ivs: IVs
+  pid: number
+}
+
+export const generateStaticSymbol = (scheme: SchemeName) => {
+  const genPID = generatePID()
+  const genIVs = generateIVs(scheme)
+
+  return (seed: LCG): [IndividualBase, LCG] => {
+    const lcg = Ref.from(seed)
+    const pid = lcg.apply(genPID)
+    const ivs = lcg.apply(genIVs)
+
+    return [{ pid, ivs }, lcg.unwrap()]
+  }
+}
---
+++ app/domain/gba/generators/pid.ts
@@ -0,0 +1,30 @@
+import { Ref } from '@util/ref'
+import { getRand, type LCG } from '../lcg'
+
+type Options = {
+  condition?: (pid: number) => boolean
+}
+
+export const generatePID = (options?: Options) => (options?.condition ? withCondition(options.condition) : single)
+
+const single = (seed: LCG): [number, LCG] => {
+  const lcg = Ref.from(seed)
+  const lid = lcg.apply(getRand)
+  const hid = lcg.apply(getRand) << 16
+
+  const pid = (hid | lid) >>> 0
+
+  return [pid, lcg.unwrap()]
+}
+const withCondition =
+  (condition: (pid: number) => boolean) =>
+  (seed: LCG): [number, LCG] => {
+    const lcg = Ref.from(seed)
+
+    let pid = lcg.apply(single)
+    while (!condition(pid)) {
+      pid = lcg.apply(single)
+    }
+
+    return [pid, lcg.unwrap()]
+  }
